f83bb113a5099dea944f5eaae676371485fb4378
yxl989458 <1835800967@qq.com>
2024-02-03 12:26:34 +0800

    fix: textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,14 +9,14 @@ const InputTextear = ({ inputSendMessage }: InputTextearProps) => {
     const classNames = {
         start: {
             button: [],
-            boxContainerClass: ['items-center', 'duration-200', 'transition-all', 'border-2', 'flex', 'w-full', 'gap-5', 'outline-none', 'p-2', 'focus:outline-none', 'focus:ring-borderMain', 'font-sans', 'bg-[#fcfcf9]'],
-            textareaClass: ['placeholder:truncate', 'max-h-[190px]', 'duration-200', 'transition-all', 'focus:outline-none', ' flex-1', 'md:text-2xl', 'bg-transparent', 'resize-none'],
-            sendClass: ['flex', 'gap-5', 'bg-[#fcfcf9]']
+            boxContainerClass: ['items-center', 'duration-200', 'transition-all', 'border-2', 'flex', 'w-full', 'gap-5', 'outline-none', 'p-1 px-2', 'focus:outline-none', 'focus:ring-borderMain', 'font-sans', 'bg-[#fcfcf9]'],
+            textareaClass: ['md:placeholder:leading-8', 'placeholder:leading-8', 'max-h-[190px]', 'duration-200', 'transition-all', 'focus:outline-none', ' flex-1', 'md:text-2xl', 'bg-transparent', 'resize-none'],
+            sendClass: ['flex', 'items-center', 'gap-5', 'bg-[#fcfcf9]']
         },
         end: {
             button: ['flex', 'dark:bg-offsetDark', 'rounded-l-lg', 'col-start-1', 'row-start-2', '-ml-2'],
             boxContainerClass: ['duration-200', 'transition-all', 'bg-[#fcfcf9]', 'p-4', 'items-center', 'grid', 'grid-rows-1fr-auto', 'grid-cols-3', 'w-full', 'outline-none', 'focus:outline-none', 'gap-5'],
-            textareaClass: ['placeholder:truncate', 'col-start-1', 'col-end-4', 'pb-sm', 'overflow-auto', 'outline-none', 'w-full', 'font-sans', 'caret-superDuper', 'resize-none', 'bg-background', 'text-textMain', 'placeholder-textOff', 'max-h-[190px]', 'focus:outline-none', ' flex-1', 'md:text-2xl', 'bg-transparent', 'resize-none'],
+            textareaClass: ['col-start-1', 'col-end-4', 'pb-sm', 'overflow-auto', 'outline-none', 'w-full', 'font-sans', 'caret-superDuper', 'resize-none', 'bg-background', 'text-textMain', 'placeholder-textOff', 'max-h-[190px]', 'focus:outline-none', ' flex-1', 'md:text-2xl', 'bg-transparent', 'resize-none'],
             sendClass: ['flex', 'bg-[#fcfcf9]', 'items-center', 'justify-self-end', 'bg-background', 'rounded-full', 'space-x-2', 'col-start-3', 'row-start-2', '-mr-2']
         }
     }
@@ -29,7 +29,7 @@ const InputTextear = ({ inputSendMessage }: InputTextearProps) => {
         if (width <= 450) {
             textarea.style.height = 32 + 'px';
         } else {
-            textarea.style.height = 32 + 'px';
+            textarea.style.height = 36 + 'px';
         }
         textarea.style.height = textarea.scrollHeight + 'px';
         textarea.scrollTop = 0;
@@ -44,9 +44,9 @@ const InputTextear = ({ inputSendMessage }: InputTextearProps) => {
                                 <Icon icon="material-symbols:add-circle-outline" width={32} height={32} color="#39474a" />
                             </Button>
                         </div>
-                        <textarea style={{ height: 32, maxHeight: '290px' }} placeholder="Ask follow-up questions" className={inputValue.length <= 30 ? classNames.start.textareaClass.join(' ') : classNames.end.textareaClass.join(' ')} value={inputValue} onChange={handleInputChange} />
+                        <textarea style={{ height: 32, maxHeight: '290px' }} placeholder="Ask follow-up questions" className={inputValue.length <= 30 ? classNames.start.textareaClass.join(' ') : classNames.end.textareaClass.join(' ')} value={inputValue} onInput={handleInputChange} />
                         <div className={inputValue.length <= 30 ? [...classNames.start.sendClass,].join(' ') : classNames.end.sendClass.join(' ')}>
-                            <Switch defaultSelected color="default" className="md:block hidden">
+                            <Switch color="default" className="md:block hidden">
                                 <span className="text-xl font-bold">Copilot</span>
                             </Switch>
                             <Button onClick={() => {
