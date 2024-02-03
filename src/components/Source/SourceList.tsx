40daaa4d2a485012c5664e0537ca0a2e2fc2fd75
yxl989458 <1835800967@qq.com>
2024-02-03 17:55:02 +0800

    feat: fix: source list dom key

--- src/components/Source/SourceList.tsx
+++ src/components/Source/SourceList.tsx
@@ -10,7 +10,7 @@ const SourceList = ({ sourceList, clickSourceMore }: SourceListProps) => {
     return (
         <div className="grid md:grid-cols-4  gap-3  grid-cols-2">
             {
-                sourceList.map((source) => (<SourceCard source={source} key={source.id} />))
+                sourceList.map((source, index) => (<SourceCard source={source} key={index} />))
             }
             <SourceMore clickSourceMore={clickSourceMore} />
         </div>
