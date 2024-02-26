8e8e32a74f8c0fe618360853c0113357681b4d25
yxl989458 <1835800967@qq.com>
2024-02-26 18:33:17 +0800

    chore: update npm and vitest add config and eslint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
-
+import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig((configEnv) =>
     mergeConfig(
         viteConfig(configEnv),
+
         defineConfig({
+            plugins: [tsconfigPaths()],
             test: {
+                globals: true,
+                environment: 'jsdom',
                 exclude: ["**/node_modules/**"],
             },
         })
