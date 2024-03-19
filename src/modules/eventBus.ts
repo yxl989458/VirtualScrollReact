808463fafd36c0c3c72bdf4d945775dc20ba5ed9
yxl989458 <1835800967@qq.com>
2024-03-19 17:12:59 +0800

    perf： eventBus

--- src/modules/eventBus.ts
+++ src/modules/eventBus.ts
@@ -1,42 +1,48 @@
-type events = Record<string, (...arg: unknown[]) => void>
-type type = keyof events
-interface EventBusType {
-    events: events[];
-    emit: (type: type, ...arg: unknown[]) => void;
-    on: (type: type, callback: (...arg: unknown[]) => void) => void;
-    off: (type: type, callback: (...arg: unknown[]) => void) => void;
+type EventMap = {
+    [key: string]: (...args: unknown[]) => void;
+};
+
+export class EventBus {
+    private eventMap: EventMap[] = [];
+
+    public on<T extends keyof EventMap>(
+        type: T,
+        callback: EventMap[T]
+    ): void {
+        if (this.eventMap.some((event) => Object.prototype.hasOwnProperty.call(event, type))) {
+            console.warn(`Event ${type} is already registered`);
+            const index = this.eventMap.findIndex(
+                (event) => Object.prototype.hasOwnProperty.call(event, type)
+            );
+            this.eventMap[index][type] = callback;
+            return;
+        }
+        this.eventMap.push({ [type]: callback });
+    }
+
+    public emit<T extends keyof EventMap>(
+        type: T,
+        ...args: Parameters<EventMap[T]>
+    ): void {
+        const events = this.eventMap.filter(
+            (event) => Object.prototype.hasOwnProperty.call(event, type)
+        );
+        if (events.length === 0) {
+            throw new Error(`Event ${type} is not found`);
+        }
+        events.forEach((event) => event[type].apply(this, args));
+    }
+
+    public off<T extends keyof EventMap>(type: T): void {
+        const index = this.eventMap.findIndex(
+            (event) => Object.prototype.hasOwnProperty.call(event, type)
+        );
+        if (index === -1) {
+            throw new Error(`Event ${type} is not found`);
+        }
+        this.eventMap.splice(index, 1);
+    }
 }
-
-class EventBus implements EventBusType {
-    events: events[] = [];
-    on(type: type, callBack: (...args: unknown[]) => void) {
-
-        if (this.events.some((event) => Object.keys(event).includes(type))) {
-            console.warn("event already registered for " + type);
-            const index = this.events.findIndex((event) => Object.keys(event).includes(type))
-            this.events[index][type] = callBack
-            return
-        }
-        this.events.push({ [type]: callBack });
-    }
-    emit(type: string, ...arg: unknown[]) {
-        if (this.events.some((event) => Object.keys(event).includes(type))) {
-            this.events.filter((event) => Object.keys(event).includes(type))
-                .forEach((event) => event[type].apply(this, arg));
-            return
-        }
-        throw new Error(`Event ${type} not found`)
-
-    }
-    off(type: string) {
-        if (this.events.some((event) => Object.keys(event).includes(type))) {
-            this.events = this.events.filter((event) => !Object.keys(event).includes(type))
-            return
-        }
-        throw new Error(`Event ${type} not found`)
-    }
-}
-
 
 export default new EventBus();
 
